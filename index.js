61172991d2f172feafd5c1ea6e93bdf84c8ce659
Taha Özcan <tahaozcan@gmail.com>
2025-09-11 17:58:55 +0300

    Update score header

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 let homeScoreBase = document.getElementById("home-score")
 let guestScoreBase = document.getElementById("guest-score")
 let homeScore = 0
-let guestScore = 0 
+let guestScore = 0
 
-function IncreaseScore(team,points) {
-    if(team==="home") {
-    homeScore+=points
-    homeScoreBase.innerHTML=homeScore
+function IncreaseScore(team, points) {
+    if (team === "home") {
+        homeScore += points
+        homeScoreBase.innerHTML = homeScore
     }
-    else if (team==="guest"){
-        guestScore+=points
-        guestScoreBase.innerHTML=guestScore
+    else if (team === "guest") {
+        guestScore += points
+        guestScoreBase.innerHTML = guestScore
     }
     highlightWinner()
-    }
-    
+}
+
 function newGame() {
     homeScore = 0
     guestScore = 0
@@ -22,16 +22,16 @@ function newGame() {
     homeScoreBase.innerHTML = homeScore
     homeScoreBase.classList.remove("winner");
     guestScoreBase.classList.remove("winner");
-    }
-    
+}
+
 function highlightWinner() {
     homeScoreBase.classList.remove("winner");
     guestScoreBase.classList.remove("winner");
-    if (homeScore > guestScore){
+    if (homeScore > guestScore) {
         homeScoreBase.classList.add("winner")
     }
-    else if(guestScore > homeScore) {
+    else if (guestScore > homeScore) {
         guestScoreBase.classList.add("winner")
     }
-    
+
 }
